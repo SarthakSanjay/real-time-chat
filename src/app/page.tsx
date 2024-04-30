592a43fb476eb763d72cc3022c9293d5306186e2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-30 23:43:39 +0530

    added siderbar and chat window to the main app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import ChatWindow from "@/components/ChatWindow";
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="bg-black h-screen w-screen flex justify-center items-center">
-      REAL TIME CHAT APP
+   <div className="bg-black h-screen w-screen flex text-white">
+    <Sidebar />
+    <ChatWindow />
    </div>
   );
 }
