a700072a116e69583dd4f1a4513a6f85edb64118
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-04-30 23:41:55 +0530

    added chat window component

---
+++ src/components/ChatWindow.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ScrollArea } from './ui/scroll-area'
+
+const ChatWindow = () => {
+  return (
+    <ScrollArea className="h-full w-full rounded-md border">
+        Chat window
+    </ScrollArea>
+  )
+}
+
+export default ChatWindow
