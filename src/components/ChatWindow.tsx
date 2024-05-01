b625e114e02fc865722285e908bb51a2ac1638fb
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-05-01 23:39:29 +0530

    included input bar and test user message

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import { ScrollArea } from './ui/scroll-area'
+import MessageInputBar from './MessageInputBar'
+import { Button } from './ui/button'
+import Message from './Message'
 
 const ChatWindow = () => {
+  const messageData = [
+    {user : "sharko" , message : 'hello'},
+    {user : "marko" , message : 'what'},
+    {user : "sharko" , message : 'nothing'},
+    {user : "marko" , message : 'so'},
+    {user : "sharko" , message : 'I dont know'},
+    {user : "sharko" , message : 'wow'},
+  ]
   return (
-    <ScrollArea className="h-full w-full rounded-md border">
+    <ScrollArea className="h-full w-full rounded-md border px-10">
         Chat window
+        {
+          messageData.map((message)=>{
+            return <div className={`w-full flex items-center
+              ${message.user === "sharko" ? "justify-end" : "justify-start"}
+            `}>
+
+              <Message message={message} />
+            </div> 
+          })
+        }
+        <div className='absolute bottom-10 w-full flex justify-center'>
+          <MessageInputBar />
+          <Button className='bg-transparent text-white' variant={'outline'}>Send</Button>
+        </div>
     </ScrollArea>
   )
 }
